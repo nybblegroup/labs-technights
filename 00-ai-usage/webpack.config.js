7880b20c6ccd6b9d251852c0258a131e86b80a1b
Leonardo Rodriguez <leonardo.rodriguez@nybblegroup.com>
2025-06-12 15:58:36 -0300

    Moved files

--- 00-ai-usage/webpack.config.js
+++ 00-ai-usage/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -23,7 +23,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './index.html'
     })
   ],
   devServer: {
